5ca053cb15e15cc1f71465ec35e9a9ecc25639b0
MLDODSON <mldodson@fullsail.edu>
2020-04-14 20:16:56 -0500

    site updates

---
+++ js/service.js
@@ -0,0 +1,30 @@
+if(document.images) {
+               var image1 = new Image();     // Preload an image
+               image1.src = "img/casket1.jpg";
+               var image2 = new Image();     // Preload second image
+               image2.src = "img/casket2.jpg";
+    
+               var image3 = new Image();     // Preload an image
+               image3.src = "img/suit.jpg";
+               var image4 = new Image();     // Preload second image
+               image4.src = "img/dress.jpg";
+    
+               var image5 = new Image();     // Preload an image
+               image5.src = "img/stl1.jpg";
+               var image6 = new Image();     // Preload second image
+               image6.src = "img/stl2.jpg";
+    
+               var image7 = new Image();     // Preload an image
+               image7.src = "img/truck.jpg";
+               var image8 = new Image();     // Preload second image
+               image8.src = "img/road.jpg";
+    
+               var image9 = new Image();     // Preload an image
+               image9.src = "img/computer.jpg";
+               var image10 = new Image();     // Preload second image
+               image10.src = "img/calculate.jpg";
+            }
+
+
+
+              
